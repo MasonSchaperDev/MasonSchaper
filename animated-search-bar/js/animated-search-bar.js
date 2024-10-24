096c2dc57e6d2d6c1d670fe99fac24262c5fe43f
MasonSchaper0 <167830460+MasonSchaper0@users.noreply.github.com>
2024-10-24 13:59:12 -0700

    Add files via upload

---
+++ animated-search-bar/js/animated-search-bar.js
@@ -0,0 +1,8 @@
+const searchBarEl = document.querySelector(".search-bar-container");
+
+const magnifierEl = document.querySelector(".magnifier");
+
+magnifierEl.addEventListener("click", () => {
+    searchBarEl.classList.toggle("active");
+});
+
